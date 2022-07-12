5644158ee733f35b740ba2a1d9d523c198f4c29a
Lori Henderson <lorim95@gmail.com>
2022-07-12 14:06:10 -0400

    controllers finished

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,4 +6,22 @@ class ApplicationController < Sinatra::Base
     { message: "Good luck with your project!" }.to_json
   end
 
+  get "/sundaes" do
+    Sundae.all.to_json
+  end
+
+  post "/sundaes" do
+    Sundae.create(name: params[:name], container: params[:container], topping: params[:topping], user_id: params[:user_id], flavor_id: params[:flavor_id]).to_json
+  end
+
+  update "/sundaes/:id" do
+    update_sundae = Sundae.find(params[:id])
+    update_sundae.update(name: params[:name], container: params[:container], topping: params[:topping], user_id: params[:user_id], flavor_id: params[:flavor_id]).to_json
+  end
+
+  delete "/sundaes/:id" do
+    delete_sundae = Sundae.find(params[:id])
+    delete_sundae.destroy
+  end
+
 end
